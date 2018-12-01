48965f652c87a238b3a853b6613f1d80d1e2fe90
avieiravalle <avieiravale@gmail.com>
2018-12-01 00:14:07 -0200

    PageObjectsV2

--- e2e/NewWine.js
+++ e2e/NewWine.js
@@ -1,72 +1,43 @@
 const constants = require("../constants/constants")
 const Wine = require('../page-objects/po-vinhos');
-
+const Helper = require("protractor-helper");
 
 describe('Should create and delete Wine', () => {
     const wine = new Wine();
 
-    beforeEach((
-        ) => browser.get('/angular-cellar/#/wines')
-    );
+    beforeEach(() => browser.get('/angular-cellar/#/wines'));
 
     it('Should Delete Wine', () => {
-        const Welcome = $('#content');
-        browser.sleep(2000);
+        expect(wine.Welcome.isPresent()).toBe(true);
 
-        expect(Welcome.isPresent()).toBe(true);
+        Helper.clickWhenClickable(wine.BtnNewWine);
+        Helper.waitForElementVisibility(wine.BtnDelete)
 
-     
-        const BtnNewWine = element(by.buttonText('New Wine'));   
-        const BtnDelete = element(by.buttonText('Delete'));
-        const NomeDoVinho = $('[name="wine.name"]')
-        const Uvas = $('[name="wine.grapes"]');
-        const Pais = $('[name="wine.country"]');
-        const Regiao = $('[name="wine.region"]');
-        const Ano = $('[name="wine.year"]');
+        Helper.fillFieldWithTextWhenVisible(wine.NomeDoVinho, constants.Vinho)
+        wine.Uvas.sendKeys(constants.Uva);
+        wine.Pais.sendKeys(constants.Pais);
+        wine.Regiao.sendKeys(constants.Region);
+        wine.Ano.sendKeys(constants.Ano)
 
-        BtnNewWine.click();
-        browser.sleep(2000);
-        expect(BtnDelete.isPresent()).toBeTruthy();
-
-        NomeDoVinho.sendKeys(constants.Vinho);
-        Uvas.sendKeys(constants.Uva);
-        Pais.sendKeys(constants.Pais);
-        Regiao.sendKeys(constants.Region);
-        Ano.sendKeys(constants.Ano)
-
-        BtnDelete.click();
-        expect(Welcome.isPresent()).toBe(true);
+        wine.BtnDelete.click();
+        expect(wine.Welcome.isPresent()).toBe(true);
 
     });
 
     it('Should create Wine', () => {
-        const Welcome = $('#content');
-        
-        expect(Welcome.isPresent()).toBeTruthy();
+        expect(wine.Welcome.isPresent()).toBeTruthy();
 
-        const BtnNewWine = element(by.buttonText('New Wine'));
-        const BtnSave = element(by.buttonText('Save'));
-        const NomeDoVinho = $('[name="wine.name"]')
-        const Uvas = $('[name="wine.grapes"]');
-        const Pais = $('[name="wine.country"]');
-        const Regiao = $('[name="wine.region"]');
-        const Ano = $('[name="wine.year"]');
+        wine.BtnNewWine.click();
+        expect(wine.BtnSave.isPresent()).toBeTruthy();
 
-        BtnNewWine.click();
-        browser.sleep(2000);
-    
-        expect(BtnSave.isPresent()).toBeTruthy();
+        wine.NomeDoVinho.sendKeys(constants.Vinho);
+        wine.Uvas.sendKeys(constants.Uva);
+        wine.Pais.sendKeys(constants.Pais);
+        wine.Regiao.sendKeys(constants.Region);
+        wine.Ano.sendKeys(constants.Ano);
 
-        NomeDoVinho.sendKeys(constants.Vinho);
-        Uvas.sendKeys(constants.Uva);
-        Pais.sendKeys(constants.Pais);
-        Regiao.sendKeys(constants.Region);
-        Ano.sendKeys(constants.Ano);
-
-        BtnSave.click();
-        expect(Welcome.isPresent()).toBeTruthy();
-
-
+        wine.BtnSave.click();
+        expect(wine.Welcome.isPresent()).toBeTruthy();
     });
 
 })
--- page-objects/po-vinhos.js
+++ page-objects/po-vinhos.js
@@ -1,11 +1,22 @@
 class Wine {
-    constructor() {
-      this.relativeUrl = '';
+  constructor() {
+    this.Ano = $('[name="wine.year"]');
 
+    this.BtnDelete = element(by.buttonText('Delete'));
+    this.BtnNewWine = element(by.buttonText('New Wine'));
+    this.BtnSave = element(by.buttonText('Save'));
+    
+    this.NomeDoVinho = $('[name="wine.name"]');
 
+    this.Pais = $('[name="wine.country"]');
 
+    this.Regiao = $('[name="wine.region"]');
 
-    }
+    this.Uvas = $('[name="wine.grapes"]');
+
+    this.Welcome = $('#content');
+  
+  }
 }
 
 module.exports = Wine;
